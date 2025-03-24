d9048c92106da0228059e1da3f192a6109f57550
Raja-saga <rajaganesh18904@gmail.com>
2025-03-24 13:57:05 +0530

    first committed

---
+++ days.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class days {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter day");
+        int day = input.nextInt();
+/*        switch (day){
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+                System.out.println("weekday");
+                break;
+            case 5:
+            case 6:
+                System.out.println("weekend");
+                break;
+            default:
+                System.out.println("invalid");
+
+        }*/
+
+        switch (day){
+            case 1,2,3,4-> System.out.println("weekday");
+            case 5,6-> System.out.println("weekend");
+            default -> System.out.println("Ivalid");
+        }
+
+    }
+
+}
